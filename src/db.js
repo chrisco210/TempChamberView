45af58f3a72e83c91f450c62fd32992db27eff86
chrisco210 <chrisrachlinski@gmail.com>
2018-08-02 13:27:18 -0400

    add bcrypt support

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
-var sqlite = require('sqlite3').verbose();
+const sqlite = require('sqlite3').verbose();
 const fs = require('fs');
 
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 
 const PRODUCTION = false;
 const DB_PATH = '.';
@@ -16,7 +18,7 @@ class DB {
      * Construct an sqlite database
      */
     constructor() {
-        this.db = new sqlite.Database(`./db-${PRODUCTION ? 'production' : 'testing'}.db`);
+        this.db = new sqlite.Database(`${DB_PATH}/db-${PRODUCTION ? 'production' : 'testing'}.db`);
     }
 
 
@@ -26,9 +28,10 @@ class DB {
      */
     static dbExists() {
         return new Promise((resolve, reject) => {
-            let exists = fs.access(`${DB_PATH}/db-${PRODUCTION ? 'production' : 'testing'}.db`, (err) => {
+            fs.access(`${DB_PATH}/db-${PRODUCTION ? 'production' : 'testing'}.db`, (err) => {
+                console.error('Error finding db: ' + (err ? 'true' : 'false'));
                 if(err) {
-                    reject(false);
+                    resolve(false);
                 } else {
                     resolve(true);
                 }
@@ -84,6 +87,7 @@ class DB {
     /**
      * Insert into the table 
      * Format: INSERT INTO tableName VALUES values
+     * Do not use this for api keys - use insertApiKey
      * @param {string} tableName 
      * @param {string} values 
      * @returns {Promise} the this object of the operation
@@ -106,6 +110,7 @@ class DB {
      * Perform a select query from the database 
      * Format: SELECT resultColumn FROM table WHERE where 
      * Format (null where): SELECT resultColumn FROM table
+     * 
      * @param {string} resultColumn the  
      * @param {*} table 
      * @param {*} where 
@@ -115,19 +120,19 @@ class DB {
         return new Promise((resolve, reject) => {
             this.db.serialize(() => {
                 if(where) {
-                    this.db.run(`SELECT ${resultColumn} FROM ${table} WHERE ${where}`, [], (err) => {
+                    this.db.all(`SELECT ${resultColumn} FROM ${table} WHERE ${where}`, [], (err, rows) => {
                         if(err) {
                             reject('Error selecting. op: ' + `SELECT ${resultColumn} FROM ${table}. err: ` + err);
                         } else {
-                            resolve(this);
+                            resolve(rows);
                         }
                     });
                 } else {
-                    this.db.run(`SELECT ${resultColumn} FROM ${table}`, [], (err) => {
+                    this.db.all(`SELECT ${resultColumn} FROM ${table}`, [], (err, rows) => {
                         if(err) {
                             reject('Error selecting. op: ' + `SELECT ${resultColumn} FROM ${table}. err: ` + err);
                         } else {
-                            resolve(this);
+                            resolve(rows);
                         }
                     });
                 }     
@@ -135,13 +140,33 @@ class DB {
         });   
     }
 
-    insertKey(apiKey) {
-
+    /**
+     * Use this function to insert an api key into the database
+     * @param {string} newApiKey the api key to insert
+     */
+    insertApiKey(newApiKey) {
+        return bcrypt.hash(newApiKey, SALT_ROUNDS)
+        .then((res) => {
+            return this.insert(apiKey, res);
+        })
+        .catch((err) => {
+            console.error('Failed to insert api key: ' + err);
+        });
     }
 
     validateKey(apiKey) {
         return new Promise((resolve, reject) => {
-            
+            bcrypt.hash(apiKey, SALT_ROUNDS)
+            .then((res) => {
+                this.select('key', 'apikeys', `key = ${res}`)
+                .then((res) => {
+                    if(res.length === 0) {
+                        reject(false);
+                    } else {
+                        resolve(true);
+                    }
+                });
+            });
         });
     }
 }
